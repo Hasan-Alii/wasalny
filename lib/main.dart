17a26af2b0cfca8e7c6ce3d53f8ed1ed8a373085
Hasan_Ali <hasan.aly.dev@gmail.com>
2023-02-20 14:56:14 +0200

    5 screens added
    screens added:
    1. welcome page
    2. home page
    3. destination page
    4. payment methods 1
    5. payment methods 2
    
    along with updating assets (images, fonts)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wasalny/screens_wasalny/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: WelcomePageWidget(),
+      //const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
